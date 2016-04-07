062187a6450d485bcb529b8d5247d13deba5b980
Danilo <dan.mac22@gmail.com>
2016-04-07 16:40:27 -0300

    adicionando gráficos

---
+++ static/graphs.js
@@ -0,0 +1,98 @@
+function renderLine (id, series) {
+    var days = [];
+    unique = function(v){return days.indexOf(v) == -1}
+    for (var c = 0; c < series.length; c++) days = days.concat(Object.keys(series[c].data).filter(unique));
+    days.sort();
+
+    for (var country = 0; country < series.length; country++){
+        var newData = [], dayY = 0;
+        for (var i = 0; i < days.length; i++) {
+            unix_timestamp = Date.UTC(days[i].substr(0, 4), days[i].substr(4, 2), days[i].substr(6, 2))/1000;
+            if (series[country].data.hasOwnProperty(days[i])) dayY += series[country].data[days[i]];
+            newData.push({x: unix_timestamp, y: dayY});
+        }
+        series[country].data = newData;
+    }
+    var graph = new Rickshaw.Graph({
+        element: document.getElementById(id),
+        width: 800,
+        height: 500,
+        renderer: 'line',
+        stroke: true,
+        preserve: true,
+        series: series
+    });
+    graph.render();
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var hoverDetail = new Rickshaw.Graph.HoverDetail({
+        graph: graph,
+        xFormatter: function(x){
+            var d = new Date(x * 1000);
+            return d.getUTCDate() + ' ' + months[d.getUTCMonth()] + ' ' + d.getUTCFullYear();
+        },
+        yFormatter: function(y){return y}
+    });
+    var yAxis = new Rickshaw.Graph.Axis.Y({graph: graph});
+    yAxis.render();
+    var xAxis = new Rickshaw.Graph.Axis.Time({graph: graph});
+    xAxis.render();
+}
+function renderBar(name, id, series){
+    var c1 = name == 'earth' ? '#339966' : '#8F0000';
+    var c2 = name == 'earth' ? '#45E695' : '#E62E2E';
+    var ticks = [];
+    var width = Math.min(800, series.length * 200);
+    var height = 200;
+    var theight = height + 17
+    var maxh = Math.max.apply(null, series.map(function(v){return v.count}));
+    var graph = '<svg width="' + width + '" height="' + theight + '">';
+    var w = width / series.length;
+    var rect = function(x, h, color){
+        return '<rect x="' + w * x + '" y="' + height * (1 - h / maxh) + '" width="' + Math.floor(w / 4) + 
+            '" height="' + height * h / maxh + '" fill="' + color + '"/>'
+    };
+    for (var year = 0; year < series.length; year++){
+        graph += rect(year + 0.1, series[year].count, c1);
+        graph += rect(year + 0.2, series[year].usage , c2);
+        graph += rect(year + 0.5, series[year].usercount, '#2266AA');
+        graph += rect(year + 0.6, series[year].userreg, '#5CA3E6');
+        ticks.push(series[year].year);
+    }
+    var xAxis = '';
+    for (var y = 0; y < ticks.length; y++){
+        xAxis += '<text x="' + String(Math.floor((y + 0.5) * width / ticks.length)) + '" y="' + theight + '" text-anchor="middle">' + ticks[y] + '</text>';
+    }
+    document.getElementById(id).innerHTML += graph + xAxis + '</svg>';
+}
+
+function countryLine(d, i){
+    line = [];
+    line.push('<td><span style="display:inline-block; width:40px; height:2px; border-top:4px solid ' +
+        data[d].series[i].color + '"></span></td>');
+    line.push('<td style="background-color:#F8F8F8">' + data[d].series[i].name + '</td>');
+    line.push('<td>' + String(data[d].series[i].count) + '</td>');
+    line.push('<td style="background-color:#F8F8F8">' + String(data[d].series[i].usage) + ' (' +
+        String(Math.round(100 * data[d].series[i].usage / data[d].series[i].count)) + '%)</td>');
+    line.push('<td><a href="' + location.pathname + '/' + data[d].series[i].name.replace(/ /g, '_') + '">' +
+        String(data[d].series[i].usercount) + '</a></td>');
+    line.push('<td style="background-color:#F8F8F8">' + String(data[d].series[i].userreg) + ' (' +
+        String(Math.round(100 * data[d].series[i].userreg / data[d].series[i].usercount)) + '%)</td>');
+    return '<tr>' + line.join('') + '</tr>'
+}
+
+window.onload = function(){
+    if (typeof data === 'undefined') return;
+    var countries = document.getElementById('countries');
+    if (countries) palette = new Rickshaw.Color.Palette({scheme: 'munin'});
+    for (var d = 0; d < data.length; d++){
+        if (countries){
+            for (var i = 0; i < data[d].series.length; i++){
+                data[d].series[i]['color'] = palette.color(i);
+            }
+            data[d].series.sort(function(a, b){return b.count - a.count});
+            for (var i = 0; i < data[d].series.length; i++) countries.innerHTML += countryLine(d, i);
+        }
+        if (data[d].type == 'line') renderLine(data[d].id, data[d].series);
+        else if (data[d].type == 'bar') renderBar(data[d].name, data[d].id, data[d].series);
+    }
+}
--- wikiloves.py
+++ wikiloves.py
@@ -16,6 +16,7 @@ menu = {name: sorted(e[-4:] for e in db if e[:-4] == name) for name in set(e[:-4
 mainData = {name: {e[-4:]:
     {'count': sum(db[e][c]['count'] for c in db[e]), 
      'usercount': sum(db[e][c]['usercount'] for c in db[e]),
+     'userreg': sum(db[e][c]['userreg'] for c in db[e]),
      'usage': sum(db[e][c]['usage'] for c in db[e])}
     for e in db if e[:-4] == name} for name in set(e[:-4] for e in db)}
 
@@ -35,47 +36,60 @@ def logpage():
         log = timestamp = None
     return render_template('log.html', title=u'Update log', menu=menu, time=timestamp, log=log)
 
-@app.route('/<name>')
+@app.route('/monuments', defaults={'name': 'monuments'})
+@app.route('/earth', defaults={'name': 'earth'})
 def event_main(name):
     if not db:
         return index()
     if name in mainData:
         eventName = u'Wiki Loves %s' % name.capitalize()
-        eventData = mainData[name]
-        eventData.update(contries={c: {e[-4:]:
-            {'count': db[e][c]['count'], 'usercount': db[e][c]['usercount'], 'usage': db[e][c]['usage']}
+        eventData = {name: {y: v for y, v in mainData[name].iteritems()}}
+        eventData.update(countries = {c: {e[-4:]:
+            {'count': db[e][c]['count'], 'usercount': db[e][c]['usercount'],
+                'usage': db[e][c]['usage'], 'userreg': db[e][c]['userreg']}
             for e in db if e[:-4] == name and c in db[e]}
             for c in set(c for e in db if e[:-4] == name for c in db[e])})
         return render_template('eventmain.html', title=eventName, menu=menu, name=name, data=eventData)
     else:
         return render_template('page_not_found.html', title=u'Event not found', menu=menu)
 
-@app.route('/<name>/<year>')
+@app.route('/monuments/20<year>', defaults={'name': 'monuments'})
+@app.route('/earth/20<year>', defaults={'name': 'earth'})
 def event_year(name, year):
     if not db:
         return index()
+    year = '20' + year
     event = name + year
     if event in db:
         eventName = u'Wiki Loves %s %s' % (name.capitalize(), year)
         eventData = {c: {d: db[event][c][d] for d in db[event][c] if d != 'users'} for c in db[event]}
-        return render_template('event.html', title=eventName, menu=menu, name=name, year=year, data=eventData)
+        return render_template('event.html', title=eventName, menu=menu, name=name, year=year,
+                               data=eventData, rickshaw=True)
     else:
         return render_template('page_not_found.html', title=u'Event not found', menu=menu)
 
-@app.route('/<name>/<year>/<country>')
+@app.route('/monuments/20<year>/<country>', defaults={'name': 'monuments'})
+@app.route('/earth/20<year>/<country>', defaults={'name': 'earth'})
 def users(name, year, country):
     if not db:
         return index()
+    year = '20' + year
     event = name + year
     if event in db and country in db[event]:
         eventName = u'Wiki Loves %s %s in %s' % (name.capitalize(), year, country)
-        eventUsers = db[event][country]['users']
+        eventUsers = sorted(db[event][country]['users'].items(), key=lambda i: (i[1]['count'], i[0]), reverse=True)
         return render_template('users.html', title=eventName, menu=menu, name=name, year=year,
-                               country=country, data=eventUsers)
+                               country=country, data=eventUsers, starttime=db[event][country]['start'])
     elif event in db:
         return render_template('page_not_found.html', title=u'Country not found', menu=menu)
     else:
         return render_template('page_not_found.html', title=u'Event not found', menu=menu)
+
+@app.template_filter(name='date')
+def date_filter(s):
+    if type(s) == int:
+        s = str(s)
+    return '%s.%s.%s' % (s[6:8], s[4:6], s[0:4])
 
 @app.errorhandler(404)
 def page_not_found(error):
